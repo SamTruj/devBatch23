5376e05240d5223a3f51e4f411fe9c283e223f40
SamTruj <elitemorado123@hotmail.com>
2023-11-28 21:18:40 -0600

    llevamos la mitad de los proyectos de java

---
+++ javascript/index.js
@@ -0,0 +1,74 @@
+
+/*
+var belleza = prompt('¿eres bellisimo/a? ')
+
+if (belleza.toLowerCase() == 'si') {
+    document.write('Ciertamente')
+} else if (belleza.toLowerCase() == 'no') {
+    document.write('No te creo')
+} else {
+    document.write('.... ¿que?')
+}
+
+document.write('<br><br><br>')
+///////////////////////////////////////////////////////////////////////////
+var divisible = prompt('Dame un número y te dire si es divisible entre 2')
+
+if (divisible%2 == 0){
+    document.write(divisible + ' número es divisible entre 2')
+} else {
+    document.write(divisible + ' número no es divisible entre 2')
+}
+///////////////////////////////////////////////////////////////////////////
+var par = prompt('Dame un número y te dire si es par')
+if (par%2 == 0) {
+    alert(par + ' es un número par')
+} else {
+    alert(par + ' no es un número par')
+
+}
+///////////////////////////////////////////////////////////////////////////
+var cliente = prompt('Ingresa tu numero de cliente')
+
+if (cliente == 1000) {
+    document.write('<h2>Ganaste un premio!!!!</h2>')
+} else {
+    document.write('lo sentimos cliente #' + cliente +', sigue participando')
+}
+//////////////////////////////////////////////////////////////////////////
+var primerNumero = prompt('Dame un primer número')
+var segundoNumero = prompt('Dame un segundo número')
+
+if (primerNumero<segundoNumero) {
+    document.write(primerNumero + ' es menor a ' + segundoNumero)
+} else if (primerNumero>segundoNumero){
+    document.write(segundoNumero + ' es menor a ' + primerNumero)
+}
+///////////////////////////////////////////////////////////////////////////
+var tercerNumero = prompt('Dame un tercer número')
+
+if(primerNumero > segundoNumero && primerNumero > tercerNumero){
+    document.write('El primer numero, ', primerNumero, ', es mayor')
+}else if((segundoNumero > primerNumero && segundoNumero > tercerNumero)){
+    document.write('El segundo numero, ', segundoNumero, ', es mayor')
+}else if((tercerNumero > primerNumero && tercerNumero > segundoNumero)){
+    document.write('El segundo numero, ', tercerNum, ', es mayor')
+}else if(primerNumero == segundoNumero || primerNumero == tercerNumero){
+    document.write('Me diste 2 numeros iguales!')
+}else if(segundoNumero == primerNumero || segundoNumero == tercerNumero){
+    document.write('Me diste 2 numeros iguales!')
+}
+//////////////////////////////////////////////////////////////////////////
+var dia = prompt('Ingresa un día de la semana')
+
+if (dia.toLowerCase() == 'lunes'){
+    document.write('Odio los lunes <br> - Garfield')
+} else if (dia.toLowerCase() == 'viernes'){
+    document.write('Ya me dio sed de la peligrosa')
+} else if (dia.toLowerCase() == 'sabado' || dia.toLowerCase() == 'domingo'){
+    document.write('Saca plan!!!!')
+} else {
+    document.write('Trabajando duro o durando en el trabajo?')
+}
+//////////////////////////////////////////////////////////////////////////
+*/
