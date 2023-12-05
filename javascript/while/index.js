3312fa980a5c5523ef73ef83c94f5bb811e7d71f
SamTruj <elitemorado123@hotmail.com>
2023-12-04 20:51:50 -0600

    agregados los ejercicios terminados de while

--- javascript/while/index.js
+++ javascript/while/index.js
@@ -1,3 +1,33 @@
+//1. Pedir un numero al usuario, y ostrar desde el 1 al n los multiplos de 5
+var num = prompt('Dame un numero entero:')
+n = 1
+divisibles = []
+
+while (n <= num){
+    res = n % 5
+    if(res == 0){
+        divisibles.push(n)
+    }
+    n++
+}
+
+console.log(divisibles)
+
+//2. Solicita al usuario 2 numeros entre 1 y 50. Mostrar los numeros del 1 al 50, pero con el mensaje de "loteria!" en los numeros dados por el usuario
+var num1 = prompt('Dame dos numeros enteros entre 1 y el 50. Primer numero:')
+var num2 = prompt('Dame el segundo num:')
+n = 1
+
+while (n <= 50){
+    if(n == num1 || n == num2){
+        console.log(n + " Loteria!!!!")
+    } else {
+        console.log(n)
+    }
+    n++
+}
+
+//3. Pedir al usuario numeros. Guardarlos en array hasta que este introduzca el num 0. Mostramos el array al temrinar
 /* if para ver si el string es un numero o no
 var myString= '123'
 parseInt(myString)
@@ -8,7 +38,6 @@ if(Number.isNaN(parseInt(myString))){
 }
 */
 
-/*
 var numeros = []
 var n
 
@@ -20,8 +49,9 @@ while (n != 0){
 }
 
 console.log(numeros)
-*/
- /*
+
+//4. Pedir al usuario datos, guardarlos hasta que el usuario no ingrese nada y finalmente mostrarlos
+
 var datos = ''
 var n = prompt('Dame algun dato:')
 
@@ -31,29 +61,31 @@ while(n != null && n != ''){
 }
 
 console.log(datos)
-*/
+
+//5. Pedir al usuario un dia de la semana. Mostrar alerta personalizado por cada uno. En caso de introducir domingo, mostrar "ve a descansar" por medio de alert y terminar
+
 var dia
 
 do {
     dia = (prompt("¿Qué dia es hoy?")).toLowerCase()
     switch(dia){
         case 'lunes':
-            alert('lunes')
+            alert('Sumergirme en mi propia miseria')
             break;
         case 'martes':
-            alert('martes')
+            alert('Contemplar el abismo')
             break;
         case 'miercoles':
-            alert('miercoles')
+            alert('Solucionar la hambruna mundial sin decírselo a nadie')
             break;
         case 'jueves':
-            alert('jueves')
+            alert('Danza y ejercicio')
             break;
         case 'viernes':
-            alert('viernes')
+            alert('Cena conmigo (NO CANCELAR)')
             break;
         case 'sabado':
-            alert('sabado')
+            alert('Luchar con el odio que me tengo')
             break;
         case 'domingo':
             alert('Ve a descansar')
